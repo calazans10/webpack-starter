a453d7c363afd4b0c3c33c2c700a1a3a55dc6746
Jeferson Farias Calazans <calazans10@gmail.com>
2017-11-20 21:49:21 -0200

    Update basic scripts

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,6 @@ import Greeter from './greeter';
 import '../css/app.scss';
 
 $(() => {
-  const greeter = new Greeter('Webpack');
+  const greeter = new Greeter(process.env.NODE_ENV);
   console.log(greeter.greet());
 });
