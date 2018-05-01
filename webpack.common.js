81912783668b2f28952064b0a9d78ecac9edccd9
Jeferson Farias Calazans <calazans10@gmail.com>
2018-05-01 15:39:42 -0300

    Update dependencies and devDependencies to latest version

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,16 @@
+/* eslint global-require: "off" */
+
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+
+const devMode = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: {
     main: './src/scripts/main.js',
-    vendor: [
-      'jquery'
-    ]
+    vendor: ['jquery'],
   },
   module: {
     rules: [
@@ -18,71 +20,65 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
-          }
-        }
-      }, {
+            presets: ['env'],
+          },
+        },
+      },
+      {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { importLoaders: 1, minimize: true } },
-            {
-              loader: 'postcss-loader',
-              options: {
-                ident: 'postcss',
-                plugins: () => [
-                  require('postcss-flexbugs-fixes'),
-                  require('autoprefixer')({
-                    browsers: [
-                      '>1%',
-                      'last 4 versions',
-                      'Firefox ESR',
-                      'not ie < 9',
-                    ],
-                    flexbox: 'no-2009',
-                  })
-                ],
-              },
+        use: [
+          devMode ? 'style-loader' : MiniCSSExtractPlugin.loader,
+          { loader: 'css-loader', options: { importLoaders: 1, minimize: true } },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                require('autoprefixer')({
+                  browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+                  flexbox: 'no-2009',
+                }),
+              ],
             },
-            'sass-loader'
-          ]
-        })
-      }, {
+          },
+          'sass-loader',
+        ],
+      },
+      {
         test: /\.(html)$/,
         use: [
           {
             loader: 'html-loader',
             options: {
-              minimize: true
-            }
-          }
-        ]
-      }, {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
         test: /\.(png|jpg|gif)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
-              limit: 8192
-            }
-          }
-        ]
-      }, {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-        use: ['file-loader']
-      }
-    ]
+        use: ['file-loader'],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(['build']),
-    new HtmlWebpackPlugin({template: './src/index.html'}),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
     new webpack.ProvidePlugin({
       $: 'jquery',
-      jQuery: 'jquery'
-    })
-  ]
-}
+      jQuery: 'jquery',
+    }),
+  ],
+};
